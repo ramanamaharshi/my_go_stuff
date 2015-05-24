c17581f1e2a23c27e43f1a0f51be4bcffbf41162
ramanamaharshi <osterloh@source-lounge.de>
2015-05-24 23:11:29 +0200

    better prego

--- prego/prego.go
+++ prego/prego.go
@@ -60,37 +60,135 @@ func main () {
 		sOutputTemplate = aArgs[""][1];
 	}
 	
-	sPart := "";
-	iChunkSize := int64(1);
-	aChunk := make([]byte, iChunkSize);
-	iCount := 0;
 	iRead := 0;
+	iCount := 0;
+	sPart := "";
+	aNewLetter := make([]byte, 1);
 	var e error;
 	
 	oIn := bufio.NewReader(os.Stdin);
 	
 	for {
-		iRead, e = oIn.Read(aChunk);
-		if e != nil {break}
-		sAppend := fmt.Sprintf("%s", aChunk[:iRead]);
-		sPart += sAppend;
+		
+		iRead, e = oIn.Read(aNewLetter);
+		
+		sNewLetter := fmt.Sprintf("%s", aNewLetter[:iRead])
+		
 		aMatches := oRegExA.FindAllStringSubmatch(sPart, -1);
-		if len(aMatches) > 0 {
-			iCount ++;
+		aMatchIndices := oRegExA.FindAllStringSubmatchIndex(sPart, -1);
+		
+		if len(aMatches) != 0 {
+			
 			aLastMatch := aMatches[len(aMatches) - 1];
+			aLastMatchIndices := aMatchIndices[len(aMatchIndices) - 1];
+			
+			aMatchIndicesNext := oRegExA.FindAllStringSubmatchIndex(sPart + sNewLetter, -1);
+			
+			bOutput := false;
+			
+			if e != nil {
+				
+				bOutput = true;
+				
+			} else {
+				
+				if len(aMatchIndicesNext) > 0 {
+					
+					aLastMatchIndicesNext := aMatchIndicesNext[len(aMatchIndicesNext) - 1];
+					
+					if aLastMatchIndices[0] != aLastMatchIndicesNext[0] || aLastMatchIndices[1] == aLastMatchIndices[1]{
+						
+						bOutput = true;
+						
+					}
+					
+				}
+				
+			}
+			
+			if (bOutput) {
+				sOutput := sOutputTemplate;
+				for iNr , sValue := range aLastMatch {
+					sOutput = strings.Replace(sOutput, "\\" + strconv.Itoa(iNr), sValue, -1);
+				}
+				if bCount {
+					iCount ++;
+				} else if bReplace {
+					fmt.Print(oRegExA.ReplaceAllLiteralString(sPart, sOutput));
+				} else {
+					fmt.Print(sOutput + "\n");
+				}
+				sPart = sPart[(aLastMatchIndices[1]):];
+			}
+			
+		}
+		
+		if e != nil { break; }
+		
+		sPart += sNewLetter;
+		//base.Dump(sPart);
+		
+		/*
+		aLastMatch := []string{};
+		aLastMatchIndices := []int{};
+		bOutputPrevious := false;
+		
+		if e == nil {
+			
+			sPart += sChunk;
+			
+			aMatches := oRegExA.FindAllStringSubmatch(sPart, -1);
+			aMatchIndices := oRegExA.FindAllStringSubmatchIndex(sPart, -1);
+			
+			if len(aMatchIndices) == 0 {
+				bOutputPrevious = true;
+			} else {
+				aLastMatch = aMatches[len(aMatches) - 1];
+				aLastMatchIndices = aMatchIndices[len(aMatchIndices) - 1];
+				if -1 != aPreviousLastMatchIndices[0] && aLastMatchIndices[0] != aPreviousLastMatchIndices[0] {
+					bOutputPrevious = true;
+				}
+			}
+			
+		} else {
+			
+			bOutputPrevious = true;
+			
+		}
+		
+		//base.Dump(aLastMatch);
+		//base.Dump(aLastMatchIndices);
+		//base.Dump(bOutputPrevious);
+		
+		if (-1 != aPreviousLastMatchIndices[0] && bOutputPrevious) {
+			aMatch := aPreviousLastMatch;
 			sOutput := sOutputTemplate;
-			for iNr , sValue := range aLastMatch {
+			for iNr , sValue := range aMatch {
 				sOutput = strings.Replace(sOutput, "\\" + strconv.Itoa(iNr), sValue, -1);
 			}
 			if !bCount {
 				if bReplace {
 					fmt.Print(oRegExA.ReplaceAllLiteralString(sPart, sOutput));
 				} else {
-					fmt.Print("\\[\\033[7m\\]" + sOutput + "\\[\\033[0m\\]" + "\n");
+					fmt.Print(sOutput + "\n");
 				}
 			}
-			sPart = "";
+base.Dump(aPreviousLastMatchIndices[1]);
+base.Dump(sPart);
+			sPart = sPart[aPreviousLastMatchIndices[1]:];
+			aPreviousLastMatchIndices[0] = -1;
+			aPreviousLastMatch = []string{};
+			iCount ++;
 		}
+		
+		if (len(aLastMatch) > 0) {
+			aPreviousLastMatchIndices = aLastMatchIndices;
+			aPreviousLastMatch = aLastMatch;
+		}
+		
+		if e != nil { break; }
+		*/
+		
 	}
 	
 	if bReplace {
@@ -102,17 +200,6 @@ func main () {
 	}
 	
 }
-
-
-
-
-/*func aGetFlag (aArgs [string][]string, aFlagGroup []string) []string {
-	
-	aReturn := []string{};
-	
-	//for _ , sFlag in 
-	
-}*/
 
 
 
